a410281efcb97838ece5c8b8350e5c3b4e584559
Mickey248 <mickeynaja246@gmail.com>
2018-12-29 09:00:59 +0700

    input variable

--- print.py
+++ print.py
@@ -1,2 +1,20 @@
-print('Hello World')
-print('Hello World')
+money = 1000
+cost = 50
+day = 10
+total = money - cost * day
+
+print('Hello World 1')
+print(646+344)
+print('total : ')
+print(total)
+
+print('--------------------')
+
+x = 32
+y = 3
+z = x % y
+
+print(z)
+print(x == y)
+print(x > y)
+print(x != y)
