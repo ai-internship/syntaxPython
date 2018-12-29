2cf15ae9080074f190a3b694d87aab456d146890
Mickey248 <mickeynaja246@gmail.com>
2018-12-29 09:24:00 +0700

    if else

---
+++ if-lese-input.py
@@ -0,0 +1,7 @@
+want = int(input())
+money = 10000
+c =  money - want
+if want <= money:
+    print("You got Money")
+    print("Money :", money-want)
+print("End")
---
+++ input.py
@@ -0,0 +1,12 @@
+# name = input()
+# lastName = input()
+# print("Hello" ,name, lastName)
+
+# print("---------------------------------------")
+
+a = int(input())
+b = int(input())
+
+c = a + b
+print("total", c)
+print(type(c))
